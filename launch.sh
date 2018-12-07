1cc6a1e9373b89c896b48eedd6a4ab976b32aefb
marav <maravtdm@users.noreply.github.com>
2018-12-07 11:24:18 +0100

    Add files via upload

---
+++ launch.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+set -x
+# Terminate already running bar instances
+for i in polybar; do sudo pkill $i; done
+for i in pactl; do sudo pkill $i; done
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null
+	do sleep 2
+done
+
+m=$(xrandr -q | grep "HDMI1" | awk '{print $1}')
+status=$(xrandr -q | grep "HDMI1" | awk '{print $2}')
+
+if [ "$status" == "connected" ]; then
+	m="HDMI1"
+#	MONITOR=$m polybar --reload top &
+	MONITOR=$m polybar -l info --reload top > $HOME/.config/polybar/top.log 2>&1 &
+#        MONITOR=$m polybar --reload bottom &
+else
+	m="eDP1"
+#	MONITOR=$m polybar -l info --reload top > $HOME/.config/polybar/top.log 2>&1 &
+	MONITOR=$m polybar --reload top &
+#       MONITOR=$m polybar -l info --reload bottom > ~/.config/polybar/bottom.log 2>&1 &
+#        MONITOR=$m polybar --reload bottom &
+fi
