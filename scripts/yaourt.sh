7b7e15fd9644179ece00c4aa6d2dadbe0ab991e4
marav <maravtdm@users.noreply.github.com>
2018-12-10 11:32:42 +0100

    Update yaourt.sh

--- scripts/yaourt.sh
+++ scripts/yaourt.sh
@@ -6,13 +6,15 @@ update=$($wrapper -Qu | wc -l)
 #nmcli dev wifi | grep "\*" > /dev/null 2>&1
 iface_up=$(iwconfig wlp1s0 | grep ESSID | cut -d":" -f2 | cut -d"/" -f1)
 #?
+connect=$(fping arch.yourlabs.org | awk '{print $3}')
 
-if [ ${iface_up} = "off" ]; then 
-	echo ""
-else
+if [ $connect == "alive" ]; then
+#if [ ${iface_up} = "off" ]; then 
 	if [ ${update} = "0" ]; then
 		echo "0"
 	else
 		echo ${update}
 	fi
+else
+		echo ""
 fi
