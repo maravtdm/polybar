cf48fda8e948567a19b7d2529db6a42704684b2d
marav <maravtdm@users.noreply.github.com>
2018-12-10 16:29:54 +0100

    Update yaourt.sh

--- scripts/yaourt.sh
+++ scripts/yaourt.sh
@@ -2,13 +2,18 @@
 iface_up=$(iwconfig wlp1s0 | awk '/SSID/ {print $4}' | cut -d"\"" -f2)
 #?
 if [ ${iface_up} != "off" ]; then 
-	wrapper=yay
-	$wrapper -Syy >/dev/null 2>&1
-	update=$($wrapper -Qu | wc -l)
-	if [ ${update} = "0" ]; then
-		echo "0"
+	connect=$(fping arch.yourlabs.org | awk '{print $3}')
+	if [ $connect == "alive" ]; then
+		wrapper=yay
+		$wrapper -Syy >/dev/null 2>&1
+		update=$($wrapper -Qu | wc -l)
+		if [ ${update} = "0" ]; then
+			echo "0"
+		else
+			echo ${update}
+		fi
 	else
-		echo ${update}
+		echo ""
 	fi
 else
 		echo ""
