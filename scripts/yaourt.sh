28e6378bf4e9a685c77b57dd799085b92d48a2e2
marav <maravtdm@users.noreply.github.com>
2018-12-11 11:09:19 +0100

    Update yaourt.sh

--- scripts/yaourt.sh
+++ scripts/yaourt.sh
@@ -3,7 +3,7 @@ iface_up=$(iwconfig wlp1s0 | awk '/SSID/ {print $4}' | cut -d"\"" -f2)
 #?
 if [ ${iface_up} != "off" ]; then 
 	connect=$(fping arch.yourlabs.org | awk '{print $3}' >/dev/null 2>&1)
-	if [ $connect != "alive" ]; then
+	if [ $connect !== "alive" ]; then
 		echo ""
 	else
 		wrapper=yay
